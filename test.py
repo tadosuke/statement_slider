1851471670c5b553cbb8ced483248699f6b518ea
tadosuke <tadosuke@gmail.com>
2023-09-17 10:05:32 +0900

    テスト用コード

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-def func():
+def func_a():
+    a = 1
+    b = 2
+    c = 3
+    d = 4
+    e = 5
+
+    ab = a + b
+    ac = a + c
+    bc = b + c
+    de = d + e
+
+def func_b():
     a = 1
     b = 2
     c = 3
